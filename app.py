3f7deb5ad2cc415ac0aefd9aac48f57f947723a5
Manoj Shendre <manojshendre.1202@gmail.com>
2024-02-29 12:40:37 +0530

    Data transformation completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
 import sys
 
 
@@ -11,7 +12,11 @@ if __name__=="__main__":
     try:
         # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.Start_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.Start_data_ingestion()
+
+        datatransformation = DataTransformation()
+        datatransformation.initiate_data_transormation(train_data_path, test_data_path)
+
     except Exception as e:
         logging.info("Zero exception error")
         raise CustomException(e, sys)
--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -23,7 +23,7 @@ class DataIngestion:
     def Start_data_ingestion(self):
         try:
             ##reading the data from mysql
-            df=read_sql_data()
+            df=pd.read_csv(os.path.join('notebook/data/raw.csv'))
             logging.info("Reading completed mysql database")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -39,9 +39,7 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
 
-
             )
-
 
         except Exception as e:
             raise CustomException(e,sys)
