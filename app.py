b1b601a2dc533b505d86314987674f984f96d366
Manoj Shendre <manojshendre.1202@gmail.com>
2024-02-29 12:55:54 +0530

    Best model selected

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
 import sys
+from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 
 if __name__=="__main__":
@@ -14,8 +15,13 @@ if __name__=="__main__":
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.Start_data_ingestion()
 
-        datatransformation = DataTransformation()
-        datatransformation.initiate_data_transormation(train_data_path, test_data_path)
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info("Zero exception error")
--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -1,6 +1,6 @@
 import sys
 from dataclasses import dataclass
-import pickle
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
